0b1a6862ac9b49d561d6d7b5d0bf94b51304f056
lukaszluszczynski <lukaszluszczynski@10.24.2.252>
2017-05-26 18:13:11 +0200

    [NEW] - Exercise solution.

---
+++ src/Dojo/Basket.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Dojo;
+
+class Basket
+{
+    private static $DISCOUNT_TABLE = array(2 => 5, 3 => 10, 4 => 20, 5 => 25);
+    private $_collection           = array();
+
+    /**
+     * @param \Dojo\Book $item
+     */
+    public function add(Book $item) {
+        if(isset($this->_collection[$item->getName()])) {
+            $this->_collection[$item->getName()]['value']++;
+        } else {
+            $this->_collection[$item->getName()] = array('value' => 1, 'item' => $item);
+        }
+    }
+    
+    /**
+     * @return float
+     */
+    public function getCost() {
+        $cost = 0;
+        if(empty($this->_collection)){
+            return $cost;
+        }
+        foreach ($this->_collection as $item) {
+            if($item['item'] instanceof Book) {
+                $cost += $item['value'] * $item['item']->getPrice();
+            }
+        }
+        return $cost - $this->getDiscount();
+    }
+    
+    /**
+     * @return float
+     */
+    private function getDiscount() {
+        $temp = $this->_collection;
+        return $this->countCollectionDiscount($temp);
+    }
+    
+    /**
+     * @param array $collection
+     * @return float
+     */
+    private function countCollectionDiscount(array $collection) {
+        $cost = 0;
+        if(!empty($collection)) {
+            $count = count($collection);
+            foreach ($collection as $key => $item) {
+                $cost += $item['item']->getPrice();
+                --$collection[$key]['value'];
+                if($collection[$key]['value'] == 0) {
+                    unset($collection[$key]);
+                }
+            }
+            return round ( $cost * ( self::$DISCOUNT_TABLE[$count] ) / 100 , 2) + $this->countCollectionDiscount($collection);
+        }
+        return $cost;
+    }
+}
